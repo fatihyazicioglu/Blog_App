506695d8d4af160b43fb6f2b8c5e7efff4034c2c
fatihyazicioglu <fy134388@gmail.com>
2023-02-01 23:58:38 +0100

    addpostform

--- client/src/components/AddPostForm.js
+++ client/src/components/AddPostForm.js
@@ -1,5 +1,17 @@
 import React from 'react'
-
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  TextField,
+  Select,
+  Input,
+  MenuItem,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
 const AddPostForm = () => {
   return (
     <div>AddPostForm</div>
