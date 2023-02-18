2b3165a27c990eb475d58d084cbda20dca6ebf64
fatihyazicioglu <fy134388@gmail.com>
2023-02-18 11:16:15 +0100

    Add delete and update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -11,6 +11,18 @@ res.status(200).json(posts);
     }
 };
 
+export const getSinglePost = async (req, res) => {
+  try {
+    const { id: _id } = req.params;
+    const post = await Post.findById(_id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
 export const createPost = async (req, res) => {
     // const post = req.body;
     const newPost = new Post(post);
@@ -24,3 +36,27 @@ export const createPost = async (req, res) => {
     }
   };
 
+  export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+    try {
+      const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true });
+      res.json(updatedPost);
+    } catch (error) {
+      res.status(409).json({
+        message: error.message,
+      });
+    }
+  };
+  
+  export const deletePost = async (req, res) => {
+    const { id: _id } = req.params;
+    try {
+      const deletedPost = await Post.findByIdAndRemove(_id);
+      res.json(deletedPost);
+    } catch (error) {
+      res.status(409).json({
+        message: error.message,
+      });
+    }
+  };
